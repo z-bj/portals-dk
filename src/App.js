ca3f96ac7619d9e1796b1414ac5274504c4e895c
Bj'z <zbjarchi@gmail.com>
2021-08-21 11:28:54 +0100

    Feature Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import React from "react";
+import Modal from "./Modal";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <button>Display Modal</button>
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Modal extends Component {
+  render() {
+    return (
+      <div className="modal">
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
+            possimus reprehenderit, nisi blanditiis expedita voluptas deserunt
+            odit dolorem, libero nemo accusantium vel asperiores rerum autem
+            quidem placeat obcaecati distinctio? Non!
+          </p>
+          <button>close</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
